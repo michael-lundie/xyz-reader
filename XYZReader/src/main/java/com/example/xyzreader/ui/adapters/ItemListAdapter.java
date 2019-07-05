c2f8678eb16e7917d3ecf6d6f448f7ea336e6aaa
Michael Lundie <michael@lundie.io>
2019-07-05 15:43:19 +0900

    Separated adapter code into new ItemListAdapter class. + ButterKnife
    Reconfiguring ArticleListActivity to separate out logic, to improve readability, abstraction and
    logic. Implemented ButterKnife.

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/adapters/ItemListAdapter.java
@@ -0,0 +1,137 @@
+package com.example.xyzreader.ui.adapters;
+
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+import com.example.xyzreader.ui.ArticleListActivity;
+import com.example.xyzreader.ui.utils.DynamicHeightNetworkImageView;
+import com.example.xyzreader.ui.utils.ImageLoaderHelper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ViewHolder> {
+
+    private static final String TAG = ArticleListActivity.class.toString();
+
+    public interface onItemClickListener {
+        //TODO: What item?
+        void onItemClick();
+    }
+
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss");
+    // Use default locale format
+    private SimpleDateFormat outputFormat = new SimpleDateFormat();
+    // Most time functions can only handle 1902 - 2037
+    private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2,1,1);
+
+    private Cursor mCursor;
+    private final OnItemClickListener mListener;
+
+    public ItemListAdapter(Cursor cursor, OnItemClickListener listener) {
+        mCursor = cursor;
+        mListener = listener;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        mCursor.moveToPosition(position);
+        return mCursor.getLong(ArticleLoader.Query._ID);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_article, parent, false);
+
+//        final ItemListAdapter.ViewHolder vh = new ItemListAdapter.ViewHolder(view);
+//        view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(Intent.ACTION_VIEW,
+//                        ItemsContract.Items.buildItemUri(getItemId(vh.getAdapterPosition()))));
+//            }
+//        });
+        return new ViewHolder(view);
+    }
+
+    private Date parsePublishedDate() {
+        try {
+            String date = mCursor.getString(ArticleLoader.Query.PUBLISHED_DATE);
+            return dateFormat.parse(date);
+        } catch (ParseException ex) {
+            Log.e(TAG, ex.getMessage());
+            Log.i(TAG, "passing today's date");
+            return new Date();
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(ItemListAdapter.ViewHolder holder, int position) {
+        mCursor.moveToPosition(position);
+        holder.bind(mListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        final View mView;
+        @BindView(R.id.thumbnail) DynamicHeightNetworkImageView thumbnailView;
+        @BindView(R.id.article_title) TextView titleView;
+        @BindView(R.id.article_subtitle) TextView subtitleView;
+
+        ViewHolder(View view) {
+            super(view);
+            mView = view;
+            ButterKnife.bind(this,view);
+        }
+
+        void bind(final OnItemClickListener listener) {
+
+            titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+            Date publishedDate = parsePublishedDate();
+
+            if (!publishedDate.before(START_OF_EPOCH.getTime())) {
+
+                subtitleView.setText(Html.fromHtml(
+                        DateUtils.getRelativeTimeSpanString(
+                                publishedDate.getTime(),
+                                System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                                DateUtils.FORMAT_ABBREV_ALL).toString()
+                                + "<br/>" + " by "
+                                + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+            } else {
+                subtitleView.setText(Html.fromHtml(
+                        outputFormat.format(publishedDate)
+                                + "<br/>" + " by "
+                                + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+            }
+            //TODO: Temporarily using context for thumbnail ref, until using picasso.
+            thumbnailView.setImageUrl(
+                    mCursor.getString(ArticleLoader.Query.THUMB_URL),
+                    ImageLoaderHelper.getInstance(mView.getContext()).getImageLoader());
+            thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+        }
+    }
+}
